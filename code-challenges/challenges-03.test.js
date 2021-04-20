aedd5d851414554645c0f99efe1af82fec80f758
MichaelCampbell2020 <cam3000@gmail.com>
2021-04-19 20:47:17 -0700

    code challenges 1 through 5 pass

--- code-challenges/challenges-03.test.js
+++ code-challenges/challenges-03.test.js
@@ -50,6 +50,8 @@ For example, oddValues([1,2,3]) returns [1,3].
 
 const oddValues = (arr) => {
   // Solution code here...
+  let oddintegers = arr.filter(number => (number %2));
+  return oddintegers;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -62,6 +64,8 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 
 const notInFirstArray = (forbiddenValues, arr) => {
   // Solution code here...
+ let filterfirst = arr.filter(number => !forbiddenValues.includes(number));
+ return filterfirst;
 };
 
 /* ------------------------------------------------------------------------------------------------
