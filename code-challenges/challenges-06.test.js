c23d7fcd2aad9eae2326ed5ad2c577abd49b53b1
MichaelCampbell2020 <cam3000@gmail.com>
2021-04-24 13:58:56 -0700

    all tests pass

--- code-challenges/challenges-06.test.js
+++ code-challenges/challenges-06.test.js
@@ -24,6 +24,11 @@ Returns: ['dyoll', 'eimaj'];
 
 const getNames = (arr) => {
   // Solution code here...
+  const names = arr.map(i => {
+    let string = i.name.split('');
+    return string.reduce((acc, cur) => cur + acc, '');
+  });
+  return names;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -33,9 +38,7 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 ------------------------------------------------------------------------------------------------ */
 
-const appendTheEnd = (str) => {
-  // Solution code here...
-};
+const appendTheEnd = (str) => str + ' The end.';
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
@@ -50,9 +53,7 @@ appendFirstToLast(a);
 console.log(a) prints [1, 2, 3, 1]
 ------------------------------------------------------------------------------------------------ */
 
-const appendFirstToLast = (arr) => {
-  // Solution code here...
-};
+const appendFirstToLast = (arr) => arr.push(arr[0]);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
@@ -69,9 +70,7 @@ addBirthYearProperty(octavia, 1947);
 console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 ------------------------------------------------------------------------------------------------ */
 
-const addBirthYearProperty = (obj, year) => {
-  // Solution code here...
-};
+const addBirthYearProperty = (obj, year) => obj.yearBorn = year;
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5 - Stretch Goal
